corrected normalization

c_PatchDataset_sparse.py
@@ -71,7 +71,7 @@ class PatchDataset(Dataset):
 
 
         x1 = patch1.x.float()
-        x1 = f.normalize(x1,dim=1)
+        x1 = f.normalize(x1,dim=0)
         x1[:,1] = x1[:,1]*(-1)
         x1[:,0] = x1[:,0]*(-1)
         y1 = torch.tensor(patch1.y).long()
@@ -79,7 +79,7 @@ class PatchDataset(Dataset):
         #adj1 = torch.squeeze(to_dense_adj(edge_index1))
 
         x2 = patch2.x.float()
-        x2 = f.normalize(x2,dim=1)
+        x2 = f.normalize(x2,dim=0)
         y2 = torch.tensor(patch2.y).long()
         edge_index2 = patch2.edge_index.long()
         #adj2 = torch.squeeze(to_dense_adj(edge_index2))   